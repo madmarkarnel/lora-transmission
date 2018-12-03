4d357e51cb8bd1ea02297da994e5e418d7679c70
madmarkarnel <dmarkarnel@gmail.com>
2018-12-03 17:01:49 +0800

    tx update

--- m0_transmitter/m0_transmitter.ino
+++ m0_transmitter/m0_transmitter.ino
@@ -1,9 +1,103 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+/* for teensy
+#define RFM95_CS 10
+#define RFM95_RST 9
+#define RFM95_INT 2
+*/
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 433.0
+#define DATALEN 200       //max size of dummy length
+
+// Blinky on receipt
+#define LED 14
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+//initialize global variables
+char temp[DATALEN];   //lora
+uint8_t payload[RH_RF95_MAX_MESSAGE_LEN];
+char dummy_data[DATALEN] = ">>MADTA*TP:25.43*HM:70.20*";
+// uint8_t len = sizeof(buf);
+
 void setup() {
   // put your setup code here, to run once:
-
+	Serial.begin(115200);
+	init_lora();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+	send_thru_lora(dummy_data);
+	delay(1000);
+}
 
+void init_lora(){
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.println("LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);  
+}
+
+void send_thru_lora(char* radiopacket){
+  
+  int length = sizeof(payload);
+  int i=0, j=0;
+  // char temp[DATALEN];   //lora
+  // uint8_t payload[RH_RF95_MAX_MESSAGE_LEN]; //store the data
+
+  Serial.println("Sending to rf95_server");
+  // Send a message to rf95_server
+
+  //do not stack
+  for(i=0; i<200; i++){
+    payload[i] = (uint8_t)'0';
+  }
+
+  for(i=0; i<length; i++){
+    payload[i] = (uint8_t)radiopacket[i];
+  }
+
+  payload[i] = (uint8_t)'\0';
+
+  Serial.println((char*)payload);
+
+
+  Serial.println("sending payload!");
+  rf95.send(payload, length);
+
+  delay(100);  
 }
