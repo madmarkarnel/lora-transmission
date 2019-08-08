489ba1cb3467c217d0e8c8f84e42d61eaef15bcf
madmarkarnel <dmarkarnel@gmail.com>
2019-08-08 15:12:30 +0800

    -working sending message thru sim800L

---
+++ pduSMS/pduSMS.ino
@@ -0,0 +1,212 @@
+#define BAUDRATE  115200
+#define GSMBAUDRATE 9600
+#define GSMSerial Serial1
+
+char msgToSend[250];
+char gsmReply[10];
+
+String message = ("This is my first ARDUINO PDU convertion message");
+// String message = (">>MNGSA*b*016EFD2000026ED43000036EF73000046E114000056E034000066E2E4000076E144000086EEF3000096ED030000A6E0830000B6E3330000C6E1430000D6E363000*170325153449"); //here you can type in the text you want to send
+String phone_nr = ("09161640761");                                   //recipient's phone number in international format
+// String phone_nr=("+441234567");   //recipient's phone number in international format
+
+byte original_byte;
+byte newbyte;
+int pdu_length;
+int counter;
+String newphone_nr;
+int numlength;
+
+String smsCMD = ("AT+CMGS=");
+String quote = ("\"");
+String CR = ("\r");
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(BAUDRATE);
+  GSMSerial.begin(9600);
+  delay(1000);
+
+  GSMSerial.write("AT\r");
+  updateSerial();
+
+  // convertToPDU();
+  sendMessage();
+
+  flashLed(LED_BUILTIN, 5, 100);
+}
+
+void loop()
+{
+  //run forever and ever
+  // updateSerial();
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    GSMSerial.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while(GSMSerial.available()) 
+  {
+    Serial.write(GSMSerial.read());//Forward what Software Serial received to Serial Port
+  }
+}
+
+void sendMessage(){
+
+  GSMSerial.write("AT\r");
+  delay(500);
+  updateSerial();
+
+  GSMSerial.write("AT+CMGF=1\r");
+  delay(1000);
+  updateSerial();
+
+  String rawMsg = smsCMD + quote + phone_nr + quote + CR + message;
+  rawMsg.toCharArray(msgToSend, 250);
+  Serial.println(msgToSend);
+  
+  GSMSerial.write(msgToSend);
+  delay(1000);
+  updateSerial();
+
+  GSMSerial.write(26);  //ctrl Z
+  updateSerial();
+}
+
+void convertString(String stringData){
+  for (int i = 0; i< stringData.length(); i++)
+  {
+    GSMSerial.write(stringData[i]);
+  }
+}
+
+void flashLed(int pin, int times, int wait)
+{
+  for (int i = 0; i < times; i++)
+  {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+    if (i + 1 < times)
+    {
+      delay(wait);
+    }
+  }
+}
+
+void convertToPDU(){
+
+  numlength = phone_nr.length() - 1;
+  Serial.println(numlength);
+
+  if (numlength % 2 != 0)
+  {
+    phone_nr += "F";
+  }
+  Serial.println(phone_nr);
+
+  for (int x = 0; x < numlength; x = x + 2)
+  {
+    newphone_nr.concat(phone_nr.charAt(x + 2));
+    newphone_nr.concat(phone_nr.charAt(x + 1));
+  }
+  Serial.println(newphone_nr);
+
+  //getting ready to send the SMS
+  Serial.println("AT"); //optional: say hello to the modem
+  delay(3000);
+
+  int textlength = message.length() * 7 / 8; // text message length: number of characters*7/8
+  if (message.length() * 7 % 8 != 0)
+  {
+    textlength++;
+  }
+  if (numlength % 2 != 0)
+  { //if the length of phone number was uneven, it now increases by 1 because of the added "F"
+    pdu_length = (8 + (numlength + 1) / 2 + textlength);
+  }
+  else
+  { //8 bytes of parameters + (length of phonenumber)/2 + number of bytes in the text
+    pdu_length = (8 + numlength / 2 + textlength);
+  }
+
+  Serial.print("AT+CMGW="); //message will be stored in the mobile phone - AT+CMGS will send it off.
+  Serial.println(pdu_length);
+  delay(3000);
+  Serial.print("001100"); //these are some parameters which we don't need to bother about
+  if (numlength < 16)
+  { //now we have to send the byte containing the length of the phone number in HEX format
+    Serial.print(0);
+  }                             //the byte in HEX format needs to be 2 characters long; if needed we add a 0 in front, e.g. "C" will become "0C"
+  Serial.print(numlength, HEX); //length telnr - this time any additional F is not to be considered
+
+  Serial.print("63");           // philippines phone code
+  // Serial.print("91");           //international format of phone number //81 for national format
+  Serial.print(newphone_nr);    //now we send the rearranged phone number
+  Serial.print("0000AA");       //some more parameters
+  if (message.length() < 16)
+  {
+    Serial.print(0);
+  } //now a byte in HEX format indicating the length of the text. Again the byte needs to be made of 2 characters
+  Serial.print(message.length(), HEX);
+
+  counter = 0;
+  newbyte = 0;
+  for (int j = 0; j < message.length(); j++)
+  {
+    original_byte = message.charAt(j); //one by one we take each byte out of the original text
+
+    for (int i = 0; i < 7; i++)
+    {
+      newbyte = newbyte | bitRead(original_byte, i) << counter; //take the bits 0 to 6 of the old byte, shift as required for the new byte and add to the new byte
+      counter++;
+      if (counter == 8)
+      {
+        counter = 0; //if a new byte is complete, set the counter to 0
+        if (newbyte < 16)
+        {
+          Serial.print(0);
+        }                           //each byte in HEX format should consist of 2 characters, if necessary add 0 in front
+        Serial.print(newbyte, HEX); //when the new byte is finished, send it to the mobile phone
+        newbyte = 0;                // the next new byte will start again from 0
+      }
+    }
+  }
+  if (message.length() * 7 % 8 != 0)
+  { //if there are remaining bits (not enough to form a new byte), these are sent out as the last byte
+    if (newbyte < 16)
+    {
+      Serial.print(0);
+    } //each byte in HEX format should consist of 2 characters, if necessary add 0 in front
+    Serial.print(newbyte, HEX);
+  }
+  delay(3000);
+  // Serial.write(26); //this is a command telling the mobile phone that the end of the transmission has been reached
+  
+}
+
+/*
+  Read input serial
+ */
+int readSerial(char result[]) {
+  int i = 0;
+  while (1) {
+    while (Serial.available() > 0) {
+      char inChar = Serial.read();
+      if (inChar == '\n') {
+        result[i] = '\0';
+        Serial.flush();
+        return 0;
+      }
+      if (inChar != '\r') {
+        result[i] = inChar;
+        i++;
+      }
+    }
+  }
+}
