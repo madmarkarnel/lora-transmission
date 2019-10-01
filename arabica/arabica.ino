8c04016fdfe6b0f92602647ee2e8e7e26fc285ab
madmarkarnel <dmarkarnel@gmail.com>
2019-10-01 16:04:23 +0800

    Arabica Datalogger  - working all functions  - RTC alarm interrupt  - low power LoRa, ~1uA sleep mode

---
+++ arabica/arabica.ino
@@ -0,0 +1,422 @@
+#include <Wire.h>
+#include <LowPower.h>
+#include "Sodaq_DS3231.h"
+#include <SPI.h>
+#include <RH_RF95.h>
+#include <avr/dtostrf.h> // dtostrf missing in Arduino Zero/Due
+#include <EnableInterrupt.h>
+#include <FlashStorage.h>
+
+#define BAUDRATE 115200
+#define DUEBAUD 9600
+#define DUESerial Serial1
+#define RTCINTPIN 6
+#define DUETRIG 10
+#define DEBUG 1
+#define VBATPIN A7
+//for m0
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 433.0
+#define DATALEN 200        //max size of dummy length
+#define LORATIMEOUT 260000 //4 minutes 20 seconds timeout
+
+#define DISK1 0x50         //eeprom address
+#define RAININT   5        //rainfall interrupt pin
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+//initialize LoRa global variables
+char dataToSend[DATALEN]; //lora
+uint8_t payload[RH_RF95_MAX_MESSAGE_LEN];
+char streamBuffer[250];
+int customDueFlag = 0; //for data gathering
+int sendToLoRa = 0;
+
+//rain gauge reading
+byte regnInterrupt = 0;
+static unsigned long last_interrupt_time = 0;
+const unsigned int DEBOUNCE_TIME = 40;
+const unsigned int REFRESH_TIME = 4000;
+volatile int rainTips = 0;            //rain tips
+
+volatile bool OperationFlag = false;  //interrupt handler
+
+char station_name[6] = "MADTA";
+char Ctimestamp[13] = "";
+
+unsigned long timestart = 0;
+// unsigned long timenow = 0;
+uint8_t serial_flag = 0;
+uint8_t debug_flag = 0;
+
+uint16_t store_rtc = 00; //store rtc alarm
+
+typedef struct
+{
+  boolean valid;
+  char senslopeCommand[50];
+  char password[50];
+  char stationName[10];
+} Senslope;
+
+// Reserve a portion of flash memory to store an "int" variable
+// and call it "alarmStorage".
+FlashStorage(alarmStorage, int);
+FlashStorage(passCommand, Senslope);
+Senslope sensCommand;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(BAUDRATE);
+  DUESerial.begin(DUEBAUD);
+
+  Wire.begin();
+  rtc.begin();
+  init_lora();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(DUETRIG, OUTPUT);
+
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(DUETRIG, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(RTCINTPIN), wake, FALLING);
+  init_Sleep(); //initialize sleep State working!!!!
+
+  setAlarmEvery30(alarmFromFlashMem()); //sending settings
+  rf95.sleep();
+
+  Serial.println("Press anything to go DEBUG mode!");
+  unsigned long serStart = millis();
+  while (serial_flag == 0)
+  {
+    if (Serial.available())
+    {
+      debug_flag = 1;
+      delay(100);
+      Serial.println("Debug Mode!");
+      serial_flag = 1;
+    }
+    // timeOut in case walang serial na makuha in ~10 seconds
+    if ((millis() - serStart) > 10000)
+    {
+      serStart = millis();
+      serial_flag = 1;
+    }
+  }
+
+  flashLed(LED_BUILTIN, 5, 100);
+}
+
+void loop()
+{
+  while (debug_flag == 1)
+  {
+    getAtcommand();
+  }
+
+  wakeAndSleep();
+  // getAtcommand();
+}
+
+void wakeAndSleep()
+{
+  if (OperationFlag)
+  {
+    flashLed(LED_BUILTIN, 5, 100);
+
+    // Serial.println("interrupt detected");
+    // attachInterrupt(digitalPinToInterrupt(RTCINTPIN), wake, FALLING);
+
+    get_Due_Data();
+    setAlarmEvery30(alarmFromFlashMem());
+
+    delay(75);
+    rtc.clearINTStatus(); // needed to re-trigger rtc
+
+    rf95.sleep();
+    OperationFlag = false;
+  }
+  // working as of May 28, 2019
+  attachInterrupt(digitalPinToInterrupt(RTCINTPIN), wake, FALLING);
+  SysTick->CTRL &= ~SysTick_CTRL_TICKINT_Msk; //disable systick interrupt
+  LowPower.standby();                         //enters sleep mode
+  SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;  //Enabale systick interrupt
+}
+
+void init_lora()
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init())
+  {
+    Serial.println("LoRa radio init failed");
+    while (1)
+      ;
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ))
+  {
+    Serial.println("setFrequency failed");
+    while (1)
+      ;
+  }
+  Serial.print("Set Freq to: ");
+  Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+}
+
+void send_thru_lora(char *radiopacket)
+{
+  int length = sizeof(payload);
+  int i = 0, j = 0;
+  //do not stack
+  for (i = 0; i < 200; i++)
+  {
+    payload[i] = (uint8_t)'0';
+  }
+  for (i = 0; i < length; i++)
+  {
+    payload[i] = (uint8_t)radiopacket[i];
+  }
+  payload[i] = (uint8_t)'\0';
+  Serial.println((char *)payload);
+  // Serial.println("sending payload!");
+  rf95.send(payload, length); //sending data to LoRa
+  delay(100);
+}
+
+void wake()
+{
+  OperationFlag = true;
+  //detach the interrupt in the ISR so that multiple ISRs are not called
+  detachInterrupt(digitalPinToInterrupt(RTCINTPIN));
+}
+
+void build_message()
+{
+  for (int i = 0; i < DATALEN; i++)
+    dataToSend[i] = 0;
+  strncpy(dataToSend, ">>", 2);
+  // strncat(dataToSend, station_name, sizeof(station_name));
+  // strncat(dataToSend, "*TP:", 4);   strncat(dataToSend, temperature, sizeof(temperature));
+  // strncat(dataToSend, fixdataMadtb, sizeof(fixdataMadtb));
+  strncat(dataToSend, "*", 1);
+  strncat(dataToSend, Ctimestamp, sizeof(Ctimestamp));
+  // strncat(temp, "*TP:", 4);     strncat(temp, bme_tp, sizeof(bme_tp));
+  strncat(dataToSend, "<<", 2);
+
+  delay(10);
+}
+
+void flashLed(int pin, int times, int wait)
+{
+  for (int i = 0; i < times; i++)
+  {
+    digitalWrite(pin, HIGH);
+    delay(wait);
+    digitalWrite(pin, LOW);
+    if (i + 1 < times)
+    {
+      delay(wait);
+    }
+  }
+}
+
+// Measure battery voltage using divider on Feather M0
+void BatteryVoltage()
+{
+  float measuredvbat;
+  measuredvbat = analogRead(VBATPIN); //Measure the battery voltage at pin A7
+  measuredvbat *= 2;                  // we divided by 2, so multiply back
+  measuredvbat *= 3.3;                // Multiply by 3.3V, our reference voltage
+  measuredvbat /= 1024;               // convert to voltage
+  // return measuredvbat;
+  if (DEBUG == 1)
+  {
+    Serial.print("BatteryVoltage: ");
+  }
+  if (DEBUG == 1)
+  {
+    Serial.println(measuredvbat);
+  }
+}
+
+void init_Sleep()
+{
+  //working to as of 05-17-2019
+  SYSCTRL->XOSC32K.reg |= (SYSCTRL_XOSC32K_RUNSTDBY | SYSCTRL_XOSC32K_ONDEMAND); // set external 32k oscillator to run when idle or sleep mode is chosen
+  REG_GCLK_CLKCTRL |= GCLK_CLKCTRL_ID(GCM_EIC) |                                 // generic clock multiplexer id for the external interrupt controller
+                      GCLK_CLKCTRL_GEN_GCLK1 |                                   // generic clock 1 which is xosc32k
+                      GCLK_CLKCTRL_CLKEN;                                        // enable it
+  while (GCLK->STATUS.bit.SYNCBUSY)
+    ; // write protected, wait for sync
+
+  EIC->WAKEUP.reg |= EIC_WAKEUP_WAKEUPEN4; // Set External Interrupt Controller to use channel 4 (pin 6)
+
+  PM->SLEEP.reg |= PM_SLEEP_IDLE_CPU; // Enable Idle0 mode - sleep CPU clock only
+  //PM->SLEEP.reg |= PM_SLEEP_IDLE_AHB; // Idle1 - sleep CPU and AHB clocks
+  //PM->SLEEP.reg |= PM_SLEEP_IDLE_APB; // Idle2 - sleep CPU, AHB, and APB clocks
+
+  // It is either Idle mode or Standby mode, not both.
+  SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk; // Enable Standby or "deep sleep" mode
+}
+
+void get_Due_Data()
+{
+  unsigned long start = millis();
+  char command[30];
+
+  turn_ON_due();
+  delay(500);
+  // DUESerial.write("ARQCMD6T\r\n");
+  readTimeStamp();
+
+  sensCommand = passCommand.read();
+  command[0] = '\0';
+  strncpy((command), (sensCommand.senslopeCommand), (10));
+  strncat(command, "/", 1);
+  strncat(command, Ctimestamp, sizeof(Ctimestamp));
+  Serial.println(command);
+  DUESerial.write(command);
+
+  //original command
+  // sensCommand = passCommand.read();
+  // DUESerial.write(sensCommand.senslopeCommand);
+  delay(100);
+
+  while (customDueFlag == 0)
+  {
+    for (int i = 0; i < 250; ++i)
+      streamBuffer[i] = 0x00;
+    DUESerial.readBytesUntil('\n', streamBuffer, 250);
+    delay(500);
+
+    // timeOut in case walang makuhang data sa due
+    if ((millis() - start) > LORATIMEOUT)
+    {
+      start = millis();
+      no_data_from_senslope();
+      customDueFlag = 1;
+    }
+
+    if (strstr(streamBuffer, ">>"))
+    {
+      if (strstr(streamBuffer, "*"))
+      {
+        Serial.println("Getting DUE data. . .");
+
+        // removed
+        // strncat(streamBuffer, Ctimestamp, sizeof(Ctimestamp));
+        // strncat(streamBuffer, "<<", 2);
+
+        send_thru_lora(streamBuffer);
+        flashLed(LED_BUILTIN, 2, 100);
+        DUESerial.write("OK");
+      }
+      else
+      {
+        //maglagay ng counter max 5 then exit
+        Serial.println("Message incomplete");
+        DUESerial.write("NO");
+      }
+    }
+    else if (strstr(streamBuffer, "STOPLORA"))
+    {
+      Serial.println("Done getting DUE data!");
+      turn_OFF_due();
+      streamBuffer[0] = '\0';
+      flashLed(LED_BUILTIN, 4, 90);
+      customDueFlag = 1;
+    }
+  }
+  turn_OFF_due();
+  customDueFlag = 0;
+}
+
+void no_data_from_senslope()
+{
+  readTimeStamp();
+  sensCommand = passCommand.read(); //read from flash memory
+
+  Serial.println("No data from senslope");
+  streamBuffer[0] = '\0';
+  strncpy(streamBuffer, ">>", 2);
+  strncat((streamBuffer), (sensCommand.stationName), (10));
+  strncat(streamBuffer, "*NODATAFROMSENSLOPE*", 22);
+  strncat(streamBuffer, Ctimestamp, sizeof(Ctimestamp));
+  strncat(streamBuffer, "<<", 2);
+  delay(50);
+  send_thru_lora(streamBuffer);
+  delay(50);
+  customDueFlag = 1;
+}
+
+void turn_ON_due()
+{
+  Serial.println("Turning ON Custom Due. . .");
+  digitalWrite(DUETRIG, HIGH);
+  delay(100);
+}
+
+void turn_OFF_due()
+{
+  Serial.println("Turning OFF Custom Due. . .");
+  digitalWrite(DUETRIG, LOW);
+  delay(100);
+}
+
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data)
+{
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+
+  delay(5);
+}
+
+byte readEEPROM(int deviceaddress, unsigned int eeaddress)
+{
+  byte rdata = 0xFF;
+
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.endTransmission();
+
+  Wire.requestFrom(deviceaddress, 1);
+
+  if (Wire.available())
+    rdata = Wire.read();
+
+  return rdata;
+}
+
+void readRainfall() {
+  unsigned long interrupt_time = millis();
+  if (interrupt_time - last_interrupt_time > DEBOUNCE_TIME) {
+   rainTips++;
+  }
+  last_interrupt_time = interrupt_time;
+}
---
+++ arabica/rtc.ino
@@ -0,0 +1,419 @@
+#define ATCMD "AT"
+#define ATECMDTRUE "ATE"
+#define ATECMDFALSE "ATE0"
+#define OKSTR "OK"
+#define ERRORSTR "ERROR"
+
+bool ate = false;
+
+void getAtcommand()
+{
+
+  String serial_line, command;
+  int i_equals = 0;
+
+  do
+  {
+    serial_line = Serial.readStringUntil('\r\n');
+  } while (serial_line == "");
+  serial_line.toUpperCase();
+  serial_line.replace("\r", "");
+
+  // echo command if ate is set, default true
+  if (ate)
+    Serial.println(serial_line);
+
+  // get characters before '='
+  i_equals = serial_line.indexOf('=');
+  if (i_equals == -1)
+    command = serial_line;
+  else
+    command = serial_line.substring(0, i_equals);
+
+  // Serial.println(command);
+
+  if (command == ATCMD)
+    Serial.println(OKSTR);
+  else if (command == ATECMDTRUE)
+  {
+    ate = true;
+    Serial.println(OKSTR);
+  }
+  else if (command == ATECMDFALSE)
+  {
+    ate = false;
+    Serial.println(OKSTR);
+  }
+  else if (command == "A")
+  {
+    get_Due_Data();
+  }
+  else if (command == "B")
+  {
+    if (alarmFromFlashMem() == 0)
+    {
+      Serial.println("Alarm every 00 and 30 minutes.");
+    }
+    else if (alarmFromFlashMem() == 1)
+    {
+      Serial.println("Alarm every 05 and 35 minutes.");
+    }
+    else if (alarmFromFlashMem() == 2)
+    {
+      Serial.println("Alarm every 10 and 40 minutes.");
+    }
+    else if (alarmFromFlashMem() == 3)
+    {
+      Serial.println("Alarm every 15 and 45 minutes.");
+    } 
+  }
+  else if (command == "C")
+  {
+    setAlarmInterval();
+  }
+  else if (command == "D")
+  {
+    Serial.print("RTC temperature: ");
+    Serial.println(readTemp());
+  }
+  else if (command == "R")
+  {
+    readTimeStamp();
+    Serial.print("Timestamp: ");
+    Serial.println(Ctimestamp);
+  }
+  else if (command == "?")
+  {
+    printMenu();
+  }
+  else if (command == "S")
+  {
+    setupTime();
+  }
+  else if (command == "P")
+  {
+    Serial.println("Read rain tips.");
+  }  
+  else if (command == "E")
+  {
+    debug_flag = 0;
+    Serial.println("Exiting debug mode!");
+  }
+  else if (command == "F")
+  {
+    //do something
+  }
+  else if (command == "Y")
+  {
+    Serial.print("Current command: ");
+    sensCommand = passCommand.read();
+    Serial.println(sensCommand.senslopeCommand);
+    Serial.print("Sensor Name: ");
+    Serial.println(sensCommand.stationName);
+  }
+  else if (command == "Z")
+  {
+    //do something
+    changeSensCommand();
+  }
+  else
+  {
+    Serial.println("Command invalid!");
+  }
+}
+
+void printMenu()
+{
+  Serial.println(F("-------------------------------------"));
+  Serial.println(F("[?] Print this menu"));
+  Serial.println(F("[A] Sample sensor data"));
+  Serial.println(F("[B] Print sending interval settings"));
+  Serial.println(F("[C] Change LoRa sending time."));
+  Serial.println(F("[D] Read RTC temperature."));
+  Serial.println(F("[E] Exit Debug mode."));
+  Serial.println(F("[P] Read rain gauge tip."));
+  Serial.println(F("[R] Read Timestamp."));
+  Serial.println(F("[S] Set date and time."));
+  Serial.println(F("[Y] Print SENSLOPE command."));
+  Serial.println(F("[Z] Change SENSLOPE command."));
+  Serial.println(F("-------------------------------------"));
+}
+
+void print_rtcInterval()
+{
+  Serial.print("Alarm interval every: ");
+  Serial.println(store_rtc);
+  Serial.println("------------------------------------------------");
+  Serial.println("Enter alarm settings:");
+  Serial.println("[0] Alarm for every 0 and 30 minutes interval");
+  Serial.println("[1] Alarm for every 5 and 35 minutes interval");
+  Serial.println("[2] Alarm for every 10 and 40 minutes interval");
+  Serial.println("[3] Alarm for every 15 and 45 minutes interval");
+  Serial.println("------------------------------------------------");
+}
+
+void setAlarmInterval()
+{
+  int alarmSelect;
+  print_rtcInterval();
+  delay(1000);
+  while (!Serial.available())
+  {
+  }
+  if (Serial.available())
+  {
+    Serial.setTimeout(8000);
+    alarmSelect = Serial.parseInt();
+    Serial.print("From user = ");
+    Serial.println(alarmSelect);
+  }
+  // alarm_setting = alarmSelect;
+  // setAlarmEvery30(alarm_setting);
+  delay(50);
+  alarmStorage.write(alarmSelect);
+  delay(50);
+}
+
+uint8_t alarmFromFlashMem()
+{
+  int fromAlarmStorage;
+  fromAlarmStorage = alarmStorage.read();
+  return fromAlarmStorage;
+}
+
+void changeSensCommand()
+{
+  Serial.setTimeout(15000);
+  Serial.println("Insert command: ");
+  String dynaCommand = Serial.readStringUntil('\n');
+  // Serial.println(dynaCommand);
+  Serial.println("Insert sensor name: ");
+  String sensorName = Serial.readStringUntil('\n');
+  // Serial.println(sensorName);
+  dynaCommand.toCharArray(sensCommand.senslopeCommand, 50);
+  sensorName.toCharArray(sensCommand.stationName, 10);
+  passCommand.write(sensCommand); //save to flash memory
+}
+
+void setupTime()
+{
+  int MM = 0, DD = 0, YY = 0, hh = 0, mm = 0, ss = 0, dd = 0;
+  Serial.println(F("\nSet time and date in this format: YY,MM,DD,hh,mm,ss,dd[0-6]Mon-Sun"));
+  delay(50);
+
+  while (!Serial.available())
+  {
+  }
+  if (Serial.available())
+  {
+    YY = Serial.parseInt();
+    MM = Serial.parseInt();
+    DD = Serial.parseInt();
+    hh = Serial.parseInt();
+    mm = Serial.parseInt();
+    ss = Serial.parseInt();
+    dd = Serial.parseInt();
+  }
+  delay(10);
+  adjustDate(YY, MM, DD, hh, mm, ss, dd);
+  readTimeStamp();
+}
+
+void adjustDate(int year, int month, int date, int hour, int min, int sec, int weekday)
+{
+  DateTime dt(year, month, date, hour, min, sec, weekday);
+  rtc.setDateTime(dt); // adjust date-time as defined by 'dt'
+                       // Serial.println(rtc.now().getEpoch());	//debug info
+}
+
+float readTemp()
+{
+  float temp;
+  rtc.convertTemperature();
+  temp = rtc.getTemperature();
+  // Serial.print(rtc.getTemperature());
+  // dtostrf(temp, 5, 2, temperature);
+  return temp;
+}
+
+void readTimeStamp()
+{
+  DateTime now = rtc.now(); //get the current date-time
+  String ts = String(now.year());
+
+  if (now.month() <= 9)
+  {
+    ts += "0" + String(now.month());
+  }
+  else
+  {
+    ts += String(now.month());
+  }
+
+  if (now.date() <= 9)
+  {
+    ts += "0" + String(now.date());
+  }
+  else
+  {
+    ts += String(now.date());
+  }
+
+  if (now.hour() <= 9)
+  {
+    ts += "0" + String(now.hour());
+  }
+  else
+  {
+    ts += String(now.hour());
+  }
+
+  if (now.minute() <= 9)
+  {
+    ts += "0" + String(now.minute());
+  }
+  else
+  {
+    ts += String(now.minute());
+  }
+
+  if (now.second() <= 9)
+  {
+    ts += "0" + String(now.second());
+  }
+  else
+  {
+    ts += String(now.second());
+  }
+
+  ts.remove(0, 2); //remove 1st 2 data in ts
+  ts.toCharArray(Ctimestamp, 13);
+/*
+  if (DEBUG == 1)
+  {
+    Serial.print("Timestamp: ");
+  }
+  if (DEBUG == 1)
+  {
+    Serial.println(Ctimestamp);
+  }
+*/
+}
+
+//default every 10 minutes interval
+void setAlarm()
+{
+  DateTime now = rtc.now(); //get the current date-time
+
+  if ((now.minute() >= 0) && (now.minute() <= 9))
+  {
+    store_rtc = 10;
+  }
+  else if ((now.minute() >= 10) && (now.minute() <= 19))
+  {
+    store_rtc = 20;
+  }
+  else if ((now.minute() >= 20) && (now.minute() <= 29))
+  {
+    store_rtc = 30;
+  }
+  else if ((now.minute() >= 30) && (now.minute() <= 39))
+  {
+    store_rtc = 40;
+  }
+  else if ((now.minute() >= 40) && (now.minute() <= 49))
+  {
+    store_rtc = 50;
+  }
+  else if ((now.minute() >= 50) && (now.minute() <= 59))
+  {
+    store_rtc = 0;
+  }
+  rtc.enableInterrupts(store_rtc, 00); // interrupt at (m,s)
+  if (DEBUG == 1)
+  {
+    Serial.print("Next alarm: ");
+  }
+  if (DEBUG == 1)
+  {
+    Serial.println(store_rtc);
+  }
+}
+
+void setAlarmEvery30(int alarmSET)
+{
+  DateTime now = rtc.now(); //get the current date-time
+  switch (alarmSET)
+  {
+  case 0:
+  {
+    // Serial.println("set 0/30 . . .");
+    if ((now.minute() >= 0) && (now.minute() <= 29))
+    {
+      store_rtc = 30;
+    }
+    else if ((now.minute() >= 30) && (now.minute() <= 59))
+    {
+      store_rtc = 0;
+    }
+    enable_rtc_interrupt();
+    break;
+  }
+  case 1:
+  {
+    // Serial.println("set 5/35 . . .");
+    if ((now.minute() >= 0) && (now.minute() <= 34))
+    {
+      store_rtc = 35;
+    }
+    else if ((now.minute() >= 35) && (now.minute() <= 59))
+    {
+      store_rtc = 5;
+    }
+    enable_rtc_interrupt();
+    break;
+  }
+  case 2:
+  {
+    // Serial.println("set 10/40 . . .");
+    if ((now.minute() >= 0) && (now.minute() <= 39))
+    {
+      store_rtc = 40;
+    }
+    else if ((now.minute() >= 40) && (now.minute() <= 59))
+    {
+      store_rtc = 10;
+    }
+    enable_rtc_interrupt();
+    break;
+  }
+  case 3:
+  {
+    // Serial.println("set 15/45 . . .");
+    if ((now.minute() >= 0) && (now.minute() <= 44))
+    {
+      store_rtc = 45;
+    }
+    else if ((now.minute() >= 45) && (now.minute() <= 59))
+    {
+      store_rtc = 15;
+    }
+    enable_rtc_interrupt();
+    break;
+  }
+  }
+}
+
+void enable_rtc_interrupt()
+{
+  rtc.enableInterrupts(store_rtc, 00); // interrupt at (m,s)
+  if (DEBUG == 1)
+  {
+    Serial.print("Next alarm: ");
+  }
+  if (DEBUG == 1)
+  {
+    Serial.println(store_rtc);
+  }
+  readTimeStamp();
+}
