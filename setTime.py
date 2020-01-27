7e0fb996480a3b300e3518993f10caf7f0455209
madmarkarnel <dmarkarnel@gmail.com>
2020-01-27 13:44:02 +0800

    - updated to python 3

--- setTime.py
+++ setTime.py
@@ -1,54 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Aug 02 14:32:24 2019
-
+Created: Fri Aug 02 14:32:24 2019
 @author: Mark Arnel B. Domingo
+Updated: Jan 27 13:35:12 2020
 """
 import serial, time
 from datetime import datetime as dt
 
+ser = serial.Serial('COM72', 115200, timeout = 1) # change COMPORT 'COM72'
 
-ser = serial.Serial()
-#ser.port = '/dev/samplingusbport'
-ser.port = "COM56"
-ser.baudrate = 115200
-ser.bytesize = serial.EIGHTBITS 
-ser.parity = serial.PARITY_NONE 
-ser.stopbits = serial.STOPBITS_ONE 
-ser.timeout = 1            
-ser.xonxoff = False     
-ser.rtscts = False     
-ser.dsrdtr = False      
-ser.writeTimeout = 2    
+print ('Setting MCU timestamp!')
+connected = False
 
 def main():
-    print ('Setting RTC timestamp!')
-    ser.open()
-    
+    ser.write("S".encode()) #send S command to MCU to update timestamp
     time.sleep(1)
-
-    ser.write('S'.encode())
-    time.sleep(1)
-    
     timeStamp = str(dt.today().strftime("%Y,%m,%d,%H,%M,%S") + ',' + '0').encode()
-    
+    time.sleep(0.5)
     ser.write(timeStamp)
-    print("PC time: ", timeStamp.decode('ascii'))
-    
+    pcTs = timeStamp.decode('ascii')
+    print ("PC timestamp: ", pcTs)
     time.sleep(1)
-    
-    ser.write('R'.encode())
-    
-    ts = ser.readlines()
-    
-    print("RTC", ts[2].decode('ascii').strip())
-    #print(ts)
-    
-    time.sleep(0.2)
-    ser.close()
-    
-   
-    
+    readout = ser.readline().strip()
+    tsOut = readout.decode('ascii')
+    print("MCU Timestamp: ", tsOut)
+    print ("Timestamp succesfully updated!")
+    ser.flush() #flush buffer
+
 if __name__=='__main__':
     try:
 	    main()        
