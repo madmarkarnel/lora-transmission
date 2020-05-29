fbfbc37b6d81609851ef7528d1878f12252c782a
madmarkarnel <dmarkarnel@gmail.com>
2020-05-29 13:35:20 +0800

    - fixed bug in sending sms - auto connect sim network depending on stored number - faster csq reading

--- m0_sleep/gsm.ino
+++ m0_sleep/gsm.ino
@@ -1,11 +1,62 @@
-String tempServer;
-char serialData[200];
+String tempServer, regServer;
+
+/**
+ * @brief Send message to mobile number
+ * 
+ * @return success: sent, failed: not sent
+ */
+void send_thru_gsm(char *inputMessage, String serverNumber)
+{
+  String smsCMD = ("AT+CMGS=");
+  String quote = ("\"");
+  String CR = ("\r");
+  char msgToSend[250];
+  char atCmgsNo[250];
+  String incomingData = String(inputMessage);
+  incomingData.replace("\r", "");
+  incomingData.toCharArray(msgToSend, 250);
+  String rawMsg = smsCMD + quote + serverNumber + quote + CR;
+  rawMsg.toCharArray(atCmgsNo, 250);
+
+  Serial.print("Sending to '");
+  Serial.print(serverNumber);
+  Serial.print("': ");
+  Serial.println(msgToSend);
+
+  GSMSerial.write("AT+CMGF=1\r");
+  gsmReadOK();
+
+  for (int i = 0; i < 3; i++)
+  {
+    gsmSerialFlush();
+    GSMSerial.write(atCmgsNo); //AT+CMGS="639XXXXXXXXX"\r
+    delay(500);
+    if (strstr(readGSMResponse(), ">"))
+    {
+      GSMSerial.write(msgToSend);
+      delay(500);
+      GSMSerial.write(26);
+      if (readGSMResponse(), "OK")
+      {
+        Serial.println("Message sent!");
+        break;
+      }
+    }
+    else
+    {
+      Serial.println("Sending message failed!");
+      GSMSerial.write(26);  //need to clear pending gsm cmd
+      delay(500);
+    }
+  }
+}
 
 void manualGSMcmd()
 {
-  char cmdAllchar[250];
+  char cmdAllchar[80];
   String quote = ("\"");
   String CR = ("\r");
+  cmdAllchar[0] = '\0';
 
   Serial.setTimeout(15000);
   Serial.print("Insert GSM command: ");
@@ -13,20 +64,22 @@ void manualGSMcmd()
   Serial.println(manualCMD);
   String cmdAll = quote + manualCMD + quote + CR;
   cmdAll.toCharArray(cmdAllchar, sizeof(cmdAllchar));
+
+  GSMSerial.flush();
   GSMSerial.write(cmdAllchar);
-  // Serial.println(gsmReadReply());
-  // Serial.println(readGSMResponse());
-  while (GSMSerial.available() > 0)
-  {
-    processByteReply(GSMSerial.read());
-  }
+  Serial.println(readGSMResponse());
 }
 
-/* Pasrse sms if valid and execute command */
+/** Over the air commands
+ * Parse sms if valid and execute command
+ * SENSLOPE,REGISTERNUM, - register mobile number to control MCU, if not it will ignore SMS
+ * SENSLOPE,SENSORPOLL, - force data sampling
+ * SENSLOPE,SERVERNUMBER,639954645704 - change server number in flash memory
+ * SENSLOPE,?SERVERNUM, - check current server number
+ * SENSLOPE,?PASSW, - check Password
+*/
 void process_data(char *data)
 {
-  // Serial.println(data);
-
   // +CMT: "+639161640761","My Number","20/04/29,14:01:35+32"
   if (strncmp(data, "+CMT: \"+", 8) == 0)
   {
@@ -38,32 +91,82 @@ void process_data(char *data)
 
     Serial.print("Sender Number:");
     Serial.println(tempServer);
-    /*
-    Serial.print("Phone name: ");
-    Serial.println(name);
-    Serial.print("Timestamp: ");
-    Serial.println(timeStamp);
-    */
   }
-  else if (strncmp(data, "SENSLOPE,", 9) == 0)
+  else if (strncmp(data, get_password_from_flashMem(), 9) == 0)
   {
     Serial.println("Valid PASSWORD received!");
     char *smsCommand = strtok(data + 9, ",");
-    char *timestamp = strtok(NULL, ",");
+    char *serverSMS = strtok(NULL, ",");
     Serial.print("Keyword: ");
     Serial.println(smsCommand);
-    /* input sms command keyword here */
+    /* input sms command here */
     if (strstr(smsCommand, "SENSORPOLL"))
     {
-      // Serial.println(tempServer);
-      // flashLed(LED_BUILTIN, 8, 100);
-      digitalWrite(LED_BUILTIN, HIGH);
-      send_thru_gsm("LED was turned ON!", "639954645704");
+      if (regServer == tempServer)
+      {
+        flashLed(LED_BUILTIN, 4, 50);
+        get_Due_Data(get_logger_version(), regServer);
+      }
     }
     else if (strstr(smsCommand, "REGISTERNUM"))
     {
-      digitalWrite(LED_BUILTIN, LOW);
-      send_thru_gsm("LED was turned OFF!", tempServer);
+      flashLed(LED_BUILTIN, 4, 50);
+      regServer = "";
+      regServer += tempServer;
+      Serial.print("Saving this number: ");
+      Serial.println(regServer);
+      send_thru_gsm("Number Registered!", regServer);
+    }
+    else if (strstr(smsCommand, "?REGNUM"))
+    {
+      if (regServer == tempServer)
+      {
+        char serNum[50];
+        flashLed(LED_BUILTIN, 4, 50);
+        regServer.toCharArray(serNum, sizeof(serNum));
+        Serial.print("Registered Number: ");
+        Serial.println(serNum);
+      }
+    }
+    else if (strstr(smsCommand, "?SERVERNUM"))
+    {
+      if (regServer == tempServer)
+      {
+        char tempSerNum[50];
+        flashLed(LED_BUILTIN, 4, 50);
+        Serial.print("Server Number: ");
+        Serial.println(get_serverNum_from_flashMem());
+        get_serverNum_from_flashMem().toCharArray(tempSerNum, sizeof(tempSerNum));
+        send_thru_gsm(tempSerNum, regServer);
+      }
+    }
+    else if (strstr(smsCommand, "SERVERNUMBER"))
+    {
+      char messageNum[100];
+      char newServer[50];
+      if (regServer == tempServer)
+      {
+        flashLed(LED_BUILTIN, 4, 50);
+        /* save new server number from remote*/
+        strcpy(flashServerNumber.inputNumber, serverSMS);
+        newServerNum.write(flashServerNumber);
+        get_serverNum_from_flashMem().toCharArray(newServer, sizeof(newServer));
+
+        strncpy(messageNum, "New server number: ", 19);
+        strncat(messageNum, newServer, sizeof(newServer));
+        Serial.println(messageNum);
+        send_thru_gsm(messageNum, newServer);
+      }
+    }
+    else if (strstr(smsCommand, "?PASSW"))
+    {
+      if (regServer == tempServer)
+      {
+        flashLed(LED_BUILTIN, 4, 50);
+        Serial.print("Password: ");
+        Serial.println(get_password_from_flashMem());
+        send_thru_gsm(get_password_from_flashMem(), regServer);
+      }
     }
   }
 }
@@ -98,45 +201,9 @@ void processIncomingByte(const byte inByte)
 
 } // end of processIncomingByte
 
-/* Read GSM reply; non read blocking process*/
-void processByteReply(const byte inByte)
-{
-  const unsigned int MAX_INPUT = 80; // how much serial data we expect before a newline
-  static char input_line[MAX_INPUT];
-  static unsigned int input_pos = 0;
-
-  switch (inByte)
-  {
-  case '\n':                   // end of text
-    input_line[input_pos] = 0; // terminating null byte
-
-    // Serial.println(input_line);
-    // getCSQval(input_line);
-    printReply(input_line);
-
-    // reset buffer for next time
-    input_pos = 0;
-    break;
-
-  case '\r': // discard carriage return
-    break;
-
-  default:
-    // keep adding if not full ... allow for terminating null byte
-    if (input_pos < (MAX_INPUT - 1))
-      input_line[input_pos++] = inByte;
-    break;
-  } // end of switch
-}
-
-void printReply(char *buff)
-{
-  Serial.println(buff);
-}
-
 char *readGSMResponse()
 {
-  char response[200];
+  char response[100]; //200
   int length = sizeof(response);
   response[0] = '\0';
 
@@ -147,24 +214,184 @@ char *readGSMResponse()
   return response;
 }
 
-void gsmReadOK()
+bool gsmReadOK()
 {
-  for (int i = 0; i < 50; i++)
+  for (int i = 0; i < 100; i++) // 50 - 500ms
   {
     if (strstr(readGSMResponse(), "OK"))
     {
-      // Serial.println("Read OK");
+      // Serial.println("Read OK from GSM");
+      return true;
       break;
     }
     // Serial.print(" .");
     delay(10);
   }
+  return false;
 }
 
-String readGSMSerial(int maxResponseTime = 500)
+void gsmSerialFlush()
+{
+  while (GSMSerial.available() > 0)
+  {
+    char t = GSMSerial.read();
+  }
+}
+
+int getCsqStrtok(char *buffer)
+{
+  char *tmpBuf;
+  tmpBuf = strtok(buffer, ": ");
+  tmpBuf = strtok(NULL, ",");
+  return (atoi(tmpBuf));
+}
+
+String getCSQ()
+{
+  String cCSQval = ("99");
+  gsmSerialFlush();
+  GSMSerial.write("AT\r");
+  delay(500);
+  if (gsmReadOK())
+  {
+    GSMSerial.write("AT+CSQ\r");
+    delay(500);
+    String nCSQ = String(getCsqStrtok(readGSMResponse()));
+    return nCSQ;
+  }
+  else
+  {
+    return cCSQval;
+  }
+}
+
+/*  RING pin
+  - normal voltage: 2.7v
+  - when SMS received 2.7v , LOW , 2.7v
+  - when call pin is 0v, then back after to 2.7 after end call
+  *i-connect sa intrrupt pin, mabubuhay if may received na sms
+*/
+void gsmHangup()
+{
+  //when ring pin triggers, call this function
+  delay(2000);
+  GSMSerial.write("ATH\r");
+}
+
+void gsmDeleteReadSmsInbox()
+{
+  GSMSerial.write("AT+CMGD=1,2\r");
+  Serial.println(readGSMResponse());
+}
+
+/**
+ * @brief Check if mobile number network is GLOBE or SMART
+ * 
+ * @return : SMART: 63908, failed: not sent
+ * @default : GLOBE
+ */
+String simNetwork()
+{
+  char tNum[50];
+  get_serverNum_from_flashMem().toCharArray(tNum, sizeof(tNum));
+  if (strstr(tNum, "63908"))
+  {
+    return "SMART";
+  }
+  else
+  {
+    return "GLOBE";
+  }  
+}
+
+void gsmManualNetworkConnect()
+{
+  // String simNetwork = "GLOBE"; //or "SMART" , hardcoded
+  String simNetCommand = "AT+COPS=1,1,\"" + simNetwork() + "\"\r";
+  String response;
+  char command[25];
+  simNetCommand.toCharArray(command, 25);
+  Serial.println(command);
+
+  gsmSerialFlush();
+  GSMSerial.write(command);
+  delay(1000);
+  if (gsmReadOK())
+  {
+    Serial.println("OK received.");
+  }
+  else
+  {
+    Serial.println("Command failed.");
+  }
+}
+
+void sleepGSM()
+{
+  //the module enters sleep mode after 5 seconds of inactivity
+  GSMSerial.write("AT+CSCLK=2\r");
+  if (gsmReadOK())
+  {
+    Serial.println("GSM going to sleep!");
+  }
+}
+
+void wakeGSM()
+{
+  GSMSerial.write("AT\r");
+  gsmReadOK();
+  GSMSerial.write("AT+CSCLK=0\r");
+  if (gsmReadOK())
+  {
+    Serial.println("GSM is alive!");
+  }
+  else
+  {
+    Serial.println("GSM did NOT wake!");
+  }
+}
+
+void resetGSM()
+{
+  int counter = 0;
+  Serial.println("GSM resetting . . .");
+  digitalWrite(GSMRST, LOW);
+  delay(500);
+  digitalWrite(GSMRST, HIGH);
+  delay(500);
+  do
+  {
+    GSMSerial.write("AT\r");  //gsm initialization
+    Serial.println("Sending AT cmd");
+  } while ((gsmReadOK() == false));
+  Serial.println("GSM ready");
+}
+
+void turn_ON_GSM()
+{
+  digitalWrite(GSMPWR, HIGH);
+  Serial.println("Turning ON GSM . . .");
+  delay(500);
+  GSMSerial.write("AT\r");  //gsm initialization
+  gsmReadOK();
+  GSMSerial.write("ATE0\r");  //turn off echo
+  gsmReadOK();
+
+  gsmManualNetworkConnect();
+}
+
+void turn_OFF_GSM()
+{
+  delay(1000);
+  digitalWrite(GSMPWR, LOW);
+  Serial.println("Turning OFF GSM . . .");
+}
+
+/*
+String readGSMSerial(int maxResponseTime = 100)
 {
   String response;
-  int timeout = 0; // ~5 seconds
+  int timeout = 0;
   while (!GSMSerial.available() && timeout < maxResponseTime)
   {
     delay(10);
@@ -175,194 +402,6 @@ String readGSMSerial(int maxResponseTime = 500)
     response = GSMSerial.readString();
   }
   return response;
-}
-
-/**
- * @brief Send message to mobile number
- * 
- * @return success: sent, failed: not sent
- */
-void send_thru_gsm(char *inputMessage, String serverNumber)
-{
-  char msgToSend[250];
-  char atCmgsNo[250];
-  String incomingData = String(inputMessage);
-  incomingData.replace("\r", "");
-  incomingData.toCharArray(msgToSend, 250);
-
-  String smsCMD = ("AT+CMGS=");
-  String quote = ("\"");
-  String CR = ("\r");
-  String response;
-  String reply;
-
-  GSMSerial.write("AT+CMGF=1\r");
-  // gsmReadOK();
-  delay(500);
-
-  String rawMsg = smsCMD + quote + serverNumber + quote + CR;
-  rawMsg.toCharArray(atCmgsNo, 250);
-  Serial.print("Sending to '");
-  Serial.print(serverNumber);
-  Serial.print("': ");
-  Serial.println(msgToSend); //prints data to send
-
-  for (int i = 0; i < 2; i++)
-  {
-    // gsmSerialFlush();
-    GSMSerial.write(atCmgsNo); //AT+CMGS="639XXXXXXXXX"\r
-    delay(500);
-    if (strstr(readGSMResponse(), ">"))
-    {
-      GSMSerial.write(msgToSend); //message to send
-      delay(500);
-      GSMSerial.write(26); //enter
-      delay(500);
-      if (readGSMResponse(), "OK")
-      {
-        Serial.println("Message sent!");
-        break;
-      }
-    }
-    else
-    {
-      Serial.println("Sending message failed!");
-    }
-  }
-}
-
-//*buffer="AT+CSQ: 12,0" expected  buffer format
-void getCSQval(char *buffer)
-{
-  // if (strncmp(buffer, "AT+CSQ: ", 8) == 0)
-  // {
-  //   char *csqN = strtok(buffer + 8, ",");
-  //   Serial.println(csqN);
-  // }
-  // /*
-  char csqval[10];
-  String csqOut;
-  char *tmpBuf;
-
-  tmpBuf = strtok(buffer, ": ");
-  tmpBuf = strtok(NULL, ",");
-
-  csqOut = String(tmpBuf);
-  csqOut.replace(" ", "");
-  csqOut.toCharArray(csqval, 10);
-
-  Serial.print("CSQ: ");
-  Serial.println(csqval);
-  // return csqval;
-  // */
-}
-
-String getCSQ()
-{
-  String csqResponse, csqval, atReply;
-  int counter = 0;
-
-  do
-  {
-    atReply = gsmCommand("AT\r");
-    //Serial.println("AT");
-  } while ((checkOkError(atReply) < 0) && (counter++ < 5));
-
-  csqResponse = gsmCommand("AT+CSQ\r");
-  if (checkOkError(csqResponse) == 0)
-  {
-    //Serial.println(csqResponse);
-    csqval = csqResponse.substring(csqResponse.indexOf("+CSQ: ") + 6, csqResponse.indexOf(","));
-  }
-  else
-  {
-    csqval = '99';
-  }
-/*
-  GSMSerial.write("AT\r");
-  gsmReadOK();
-  GSMSerial.write("AT+CSQ\r");
-  gsmReadOK();
-  csqResponse = String(readGSMResponse());
-  if (strstr(readGSMResponse(), "OK"))
-  {
-    csqval = csqResponse.substring(csqResponse.indexOf("+CSQ: ") + 6, csqResponse.indexOf(","));
-  }
-  else
-  {
-    csqval = ("99");
-  }
-  */
-  return csqval;
-}
-
-void gsmHangup()
-{
-  //when ring pin triggers, call this function
-  delay(2000);
-  gsmCommand("ATH\r");
-}
-
-String gsmReadMessage()
-{
-  String response;
-  String message;
-  char str[1000];
-  int i = 0;
-
-  response = gsmCommand("AT+CMGR=1\r");
-  response.toCharArray(str, 1000);
-
-  char *token = strtok(str, "\r");
-  while (token != NULL)
-  {
-    if (i == 2) //third entry
-    {
-      message = token;
-      break;
-    }
-    token = strtok(NULL, "\r");
-    i++;
-  }
-
-  if (isResponse(message, "password"))
-  {
-    Serial.println("Valid message received.");
-  }
-  else
-  {
-    Serial.println("No valid message received.");
-    message = (".");
-  }
-
-  return message;
-}
-
-void gsmDeleteReadSmsInbox()
-{
-  gsmCommand("AT+CMGD=1,2\r");
-  Serial.println(readGSMResponse());
-}
-
-void gsmManualNetworkConnect()
-{
-  String simNetwork = "GLOBE"; //or "SMART" , hardcoded
-  String simNetCommand = "AT+COPS=1,1,\"" + simNetwork + "\"\r";
-  String response;
-  char command[25];
-  simNetCommand.toCharArray(command, 25);
-  gsmSerialFlush();
-  GSMSerial.write(command);
-  response = readGSMSerial(3000);
-  if (checkOkError(response) == 0)
-  {
-    Serial.println("OK received.");
-  }
-  else
-  {
-    Serial.println("Command failed.");
-    Serial.println(response);
-  }
 }
 
 int checkOkError(String response)
@@ -405,14 +444,6 @@ String gsmCommand(char *command)
   return response;
 }
 
-void gsmSerialFlush()
-{
-  while (GSMSerial.available() > 0)
-  {
-    char t = GSMSerial.read();
-  }
-}
-
 void updateSerial()
 {
   delay(500);
@@ -426,72 +457,23 @@ void updateSerial()
   }
 }
 
-void sleepGSM()
+String getCSQ()
 {
-  //the module enters sleep mode after 5 seconds of inactivity
-  String response;
-  response = gsmCommand("AT+CSCLK=2\r");
-  if (checkOkError(response) == 0)
-  {
-    Serial.println("GSM going to sleep!");
-  }
-}
+  String csqResponse, csqval, atReply;
+  int counter = 0;
 
-void wakeGSM()
-{
-  String response;
-  gsmCommand("AT\r");
-  response = gsmCommand("AT+CSCLK=0\r");
-  if (checkOkError(response) == 0)
+  GSMSerial.write("AT\r");
+  gsmReadOK();
+  csqResponse = gsmCommand("AT+CSQ\r");
+  if (checkOkError(csqResponse) == 0)
   {
-    Serial.println("GSM is alive!");
+    //Serial.println(csqResponse);
+    csqval = csqResponse.substring(csqResponse.indexOf("+CSQ: ") + 6, csqResponse.indexOf(","));
   }
+  else
+  {
+    csqval = ("99");
+  }
+  return csqval;
 }
-
-/*  RING pin
-  - normal voltage: 2.7v
-  - when SMS received 2.7v , LOW , 2.7v
-  - when call pin is 0v, then back after to 2.7 after end call
-  *i-connect sa intrrupt pin, mabubuhay if may received na sms
 */
-
-int readline(int readch, char *buffer, int len)
-{
-  static int pos = 0;
-  int rpos;
-
-  if (readch > 0)
-  {
-    switch (readch)
-    {
-    case '\r': // Ignore CR
-      break;
-    case '\n': // Return on new-line
-      rpos = pos;
-      pos = 0; // Reset position index ready for next time
-      return rpos;
-    default:
-      if (pos < len - 1)
-      {
-        buffer[pos++] = readch;
-        buffer[pos] = 0;
-      }
-    }
-  }
-  return 0;
-}
-
-char *gsmReadReply()
-{
-  serialData[0] = '\0';
-
-  while (GSMSerial.available() > 0)
-  {
-    if (readline(GSMSerial.read(), serialData, sizeof(serialData)) > 0)
-    {
-      // Serial.println(serialData);
-      return serialData;
-      break;
-    }
-  }
-}
